6dbc0ee6f8b83692d45098b4619e898b686832ad
babayaga6969007 <devashish902@gmail.com>
2023-06-10 07:21:34 +0545

    Seventh Day

---
+++ June_7.py
@@ -0,0 +1,19 @@
+# Variables and data types
+name = "John" #Variables = "Data Type(String)"
+age = 25 #Variables = "Integer"
+height = 1.75 #Variables = "Float"
+is_student = True  #Variables = "Boolean"
+
+# Displaying variable values
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student?", is_student)
+
+# Branching with if, elif, and else statements
+if age < 18:
+    print("You are a minor.")
+elif age >= 18 and age < 65:
+    print("You are an adult.")
+else:
+    print("You are a senior citizen.")
