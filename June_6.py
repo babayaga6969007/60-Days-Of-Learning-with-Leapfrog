b560764d7af3ffa9074a4aac8d588cc523e367fb
babayaga6969007 <devashish902@gmail.com>
2023-06-10 07:17:54 +0545

    Sixth Day

---
+++ June_6.py
@@ -0,0 +1,42 @@
+a = 15
+b = 10
+
+# Addition, Subtraction, Multiplication, Divide, Modulus
+result = a + b
+print("Addition:", result)
+result = a - b
+print("Subtraction:", result)
+result = a * b
+print("Multiplication:", result)
+result = a / b
+print("Division:", result)
+result = a % b
+print("Modulus:", result)
+
+
+# Greater than, Less than, Greater than or Equal, Less than or Equal
+if a > b:
+    print("a is greater than b")
+if a < b:
+    print("a is less than b")
+if a >= b:
+    print("a is greater than or equal to b")
+if a <= b:
+    print("a is less than or equal to b")
+
+# Logical operators
+x = True
+y = False
+
+# AND
+if x and y:
+    print("Both x and y are True")
+
+# OR
+if x or y:
+    print("Either x or y is True")
+
+# NOT
+if not y:
+    print("y is not True")
+
