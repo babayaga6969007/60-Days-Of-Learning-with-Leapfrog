e642c695080b8fd0212a2e966221b7e769a36d10
babayaga6969007 <devashish902@gmail.com>
2023-06-12 06:09:15 +0545

    Ninht Day

---
+++ June_9.py
@@ -0,0 +1,30 @@
+# Problem 1: John has 15 pens. He gives 3 pens to his friend and then 
+#receives 5 pens as a gift. How many pens does John have now?
+
+#Declaring and initializing the variables for problem 1
+initial_pens = 15
+pens_given_away = 3
+pens_received = 5
+
+#Performing arithmetic operations for problem 1
+remaining_pens = initial_pens - pens_given_away + pens_received
+print("Problem 1:")
+print("John has", remaining_pens, "pens now.")
+
+# Problem 2: Sarah has 25 books. She buys 7 more books and then gives
+#  away 3 books to her friend. How many books does Sarah have now?
+
+
+initial_books = 25
+books_bought = 7
+books_given_away = 3
+remaining_books = initial_books + books_bought - books_given_away
+print("\nProblem 2:")
+print("Sarah has", remaining_books, "books now.")
+
+#Wap to find area of rectangle
+length = 10
+width = 5
+area = length * width
+print("\nProblem 2:")
+print("The area of the rectangle is", area)
